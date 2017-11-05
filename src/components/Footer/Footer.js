5064ba0fa39af54d1754e280f1e93da294da249d
hamza <hz.fatmi@gmail.com>
2017-11-05 20:01:39 +0000

    refactoring footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">Copyright &copy; 2017</p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
