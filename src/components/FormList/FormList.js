ef03647819f5ca4cae3d17463c0a39baf7fa5bda
hamza <hz.fatmi@gmail.com>
2017-11-05 20:06:56 +0000

    refactoring formlist component

---
+++ src/components/FormList/FormList.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Form from "./FormComponent";
+import { render } from "react-dom";
+var request = require("request");
+
+class FormList extends Component {
+  render() {
+    return (
+      <div>
+        <section id="login">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <h2>Login</h2>
+                <Form>/login</Form>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="signup" className="bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <h2>Sign up</h2>
+                <Form>/signup</Form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default FormList;
