bc2c82e681449e7273cb95713989f83e8bd3a1f0
hamza <hz.fatmi@gmail.com>
2017-11-06 00:28:50 +0000

    fix file extension in package.json

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import Gallery from '../Gallery';
-import FormList from '../FormList';
+import React, { Component } from "react";
+import Gallery from "../Gallery";
+import FormList from "../FormList";
 
-import { render } from 'react-dom';
+import { render } from "react-dom";
 
-class Body extends Component {
-  render() {
-    var Child;
-    if (this.props.email) {
-      Child = Gallery;
-      return <Child {...this.props} />;
-    }
-
-    Child = FormList;
-    return <Child />;
+const Body = props => {
+  let Child;
+  if (props.email) {
+    Child = Gallery;
+    return <Child {...this.props} />;
   }
-}
+
+  Child = FormList;
+  return <Child />;
+};
 
 export default Body;
