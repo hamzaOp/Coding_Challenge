37f821a1158a5052fdcc9bfa765894a49ca15e44
hamza <hz.fatmi@gmail.com>
2017-11-06 18:06:29 +0000

    fix eslint errors

--- src/client.jsx
+++ src/client.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Nighthawk from 'nighthawk';
-import setupRouter from './utilities/setup_route';
-import Layout from './components/Layout';
+import React from "react";
+import { render } from "react-dom";
+import Nighthawk from "nighthawk";
+import setupRouter from "./utilities/setup_route";
+import Layout from "./components/Layout";
 
 const router = Nighthawk();
 
@@ -11,14 +11,12 @@ setupRouter(router);
 router.use((req, res, next) => {
   fetch(req.url, {
     headers: {
-      Accept: 'application/json',
-      'X-Requested-With': 'XMLHttpRequest'
+      Accept: "application/json",
+      "X-Requested-With": "XMLHttpRequest"
     },
-    credentials: 'include'
+    credentials: "include"
   })
-    .then(data => {
-      data.json();
-    })
+    .then(response => response.json())
     .then(data => {
       res.locals.currentUser = data;
       next();
@@ -26,7 +24,10 @@ router.use((req, res, next) => {
 });
 
 router.use((req, res, next) => {
-  render(<Layout {...res.locals.currentUser} />, document.getElementById('root'));
+  render(
+    <Layout {...res.locals.currentUser} />,
+    document.getElementById("root")
+  );
   next();
 });
 
